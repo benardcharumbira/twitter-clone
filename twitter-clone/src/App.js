29e4b1204e9f7449abf8a0c0662c135a70e1ba2d
Benard <benardcharumbira@gmail.com>
2020-08-14 10:27:25 +0200

    added sidebar component

--- twitter-clone/src/App.js
+++ twitter-clone/src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./App.css";
+import Sidebar from "./components/SideBar/Sidebar";
 
 function App() {
   return (
     <div className="app">
-      <h1>Hello</h1>
+      <Sidebar />
     </div>
   );
 }
