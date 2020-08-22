ffdbb166aaf4d7a489714ae935842a2106c791b3
Benard <benardcharumbira@gmail.com>
2020-08-22 10:43:37 +0200

    Updates

---
+++ precache-manifest.61b3f34c4c5d940adaabad679603a93a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "900d38189e95ff86143766fa6be94248",
+    "url": "/twitter-clone/index.html"
+  },
+  {
+    "revision": "377d44cb6ec7061c8510",
+    "url": "/twitter-clone/static/css/main.5eec152b.chunk.css"
+  },
+  {
+    "revision": "ab554ef1b1c2f9ecf630",
+    "url": "/twitter-clone/static/js/2.9d00966b.chunk.js"
+  },
+  {
+    "revision": "a8de4128b679d6159aad8871b5569c6d",
+    "url": "/twitter-clone/static/js/2.9d00966b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "377d44cb6ec7061c8510",
+    "url": "/twitter-clone/static/js/main.b73afe95.chunk.js"
+  },
+  {
+    "revision": "4ea1c8ca6c4e7890228d",
+    "url": "/twitter-clone/static/js/runtime-main.a74d6e56.js"
+  }
+]);
